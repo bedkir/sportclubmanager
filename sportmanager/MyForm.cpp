48cbfe2c02ec4143e6e86268c9355e9409ae1112
bedkir <pandorino1206@gmail.com>
2023-12-25 14:18:14 +0200

    form view

--- sportmanager/MyForm.cpp
+++ sportmanager/MyForm.cpp
@@ -1,2 +1,11 @@
 #include "MyForm.h"
-
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	sportmanager::MyForm form;
+	Application::Run(% form);
+}
